b58dc61384e26ac0a28d93e54c1e8cda9c8557cd
Kristijan Salaji <kristijansalaji20@gmail.com>
2018-06-04 12:28:49 +0200

    implemented initial model

--- initialApp-master/RentApp/Models/Entities/TypeOfVehicle.cs
+++ initialApp-master/RentApp/Models/Entities/TypeOfVehicle.cs
@@ -1,19 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Web;
 
 namespace RentApp.Models.Entities
 {
-    public class Service
+    public class TypeOfVehicle
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Logo { get; set; }
-        public string Email { get; set; }
-        public string Description { get; set; }
         public List<Vehicle> Vehicles { get; set; }
-        public List<Branch> Branches { get; set; }
     }
 }
