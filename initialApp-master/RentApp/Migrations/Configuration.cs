dcb34aa393a40104961ed1b6b95d4d9460d83364
Marko Bubnjevi? <E3116-2014@ftn.ns>
2018-06-05 08:53:28 +0200

    popunjavanje tabela osnovnim podacima

--- initialApp-master/RentApp/Migrations/Configuration.cs
+++ initialApp-master/RentApp/Migrations/Configuration.cs
@@ -4,9 +4,12 @@ namespace RentApp.Migrations
     using Microsoft.AspNet.Identity.EntityFramework;
     using RentApp.Models.Entities;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
+    using System.Data.Entity.Validation;
     using System.Linq;
+    using System.Text;
 
     internal sealed class Configuration : DbMigrationsConfiguration<RentApp.Persistance.RADBContext>
     {
@@ -29,6 +32,70 @@ namespace RentApp.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+
+          /*  if (System.Diagnostics.Debugger.IsAttached == false)
+            {
+                System.Diagnostics.Debugger.Launch();
+            }*/
+
+            Branch b1 = new Branch();               // FILIJALA
+            b1.Address = "Zivojina Culuma";
+            b1.Latitude = 1115;
+            b1.Longitude = 2225;
+            b1.Logo = "asdasd";
+
+            //context.Branches.Add(b1);               //----
+
+            TypeOfVehicle tov = new TypeOfVehicle();           // TYPE OF VECHILE
+          
+            tov.Name = "Karavan";
+            tov.Vehicles = new List<Vehicle>();
+            
+         
+
+
+            Vehicle v1 = new Vehicle();                   // VOZILO
+            v1.Description = "bla bla";
+           
+            
+            List<string> lista = new List<string>();
+            lista.Add("asdasd");
+            lista.Add("asdasdfgg");
+
+            v1.Images = lista;
+            v1.Manufactor = "Golf";
+            v1.Model = "Passat";
+            v1.PricePerHoure = 200;
+            v1.Type = tov;
+            v1.Year = 1991;
+
+            Rent r1 = new Rent();                       //RENT
+            r1.Branch = b1;
+            r1.Vehicle = v1;
+
+                
+            AppUser a1 = new AppUser();                   // APPUSER
+            a1.Activated = true;
+            a1.Email = "maxtax@gmail.com";
+            a1.FullName = "Maksimilijan Taksimovic";
+            a1.PersonalDocument = "919250126";
+            a1.Renting = new List<Rent>();
+            a1.Renting.Add(r1);
+
+            Service s1 = new Service();                //SERVIS
+            s1.Description = "servis1";
+            s1.Email = "mojservis1@gmail.com";
+            s1.Branches = new List<Branch>();
+            s1.Vehicles = new List<Vehicle>();
+            s1.Branches.Add(b1);
+            s1.Vehicles.Add(v1);
+
+
+            context.Services.Add(s1);
+
+
+
             if (!context.Roles.Any(r => r.Name == "Admin"))
             {
                 var store = new RoleStore<IdentityRole>(context);
@@ -72,7 +139,32 @@ namespace RentApp.Migrations
 
             );
 
-            context.SaveChanges();
+            // context.SaveChanges();
+
+            try
+            {
+                context.SaveChanges();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                var sb = new StringBuilder();
+                foreach (var failure in ex.EntityValidationErrors)
+                {
+                    sb.AppendFormat("{0} failed validation\n", failure.Entry.Entity.GetType());
+                    foreach (var error in failure.ValidationErrors)
+                    {
+                        sb.AppendFormat("- {0} : {1}", error.PropertyName, error.ErrorMessage);
+                        sb.AppendLine();
+                    }
+                }
+                throw new DbEntityValidationException(
+                    "Entity Validation Failed - errors follow:\n" +
+                    sb.ToString(), ex
+                );
+            }
+
+
+
 
             var userStore = new UserStore<RAIdentityUser>(context);
             var userManager = new UserManager<RAIdentityUser>(userStore);
